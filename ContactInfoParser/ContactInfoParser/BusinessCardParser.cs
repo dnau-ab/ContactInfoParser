fe66a2b0b28635d6e6df17f1cfc247998373128e
David Naughton <dnau.ab@gmail.com>
2018-03-23 11:22:49 -0500

    Base project and classes

---
+++ ContactInfoParser/ContactInfoParser/BusinessCardParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactInfoParser {
+    class BusinessCardParser {
+
+        public ContactInfo getContactInfo(String document) {
+
+            return new ContactInfo("", "", "");
+        }
+
+        // Returns full name found in given input or empty string
+        private String findName(String input) {
+
+            return String.Empty;
+        }
+
+        // Returns a phone number found in the given input or empty string
+        private String findPhoneNumber(String input) {
+
+            return String.Empty;
+        }
+
+        // Returns an email found in the given input or empty string
+        private String findEmailAddress(String input) {
+
+            return String.Empty;
+        }
+
+    }
+}
---
+++ ContactInfoParser/ContactInfoParser/ContactInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactInfoParser {
+    class ContactInfo {
+
+        private String name;
+        private String phone;
+        private String email;
+
+        public ContactInfo(String name, String phone, String email){
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        // Returns the contact's full name: "John Doe", "Bob Ross"
+        public String getName() { return name; }
+
+        // Returns the contact's phone number formatted as a sequence of digits
+        public String getPhoneNumber() { return phone; }
+
+        // Returns the contact's email address: "email123@gmail.com", "helpdesk@company.net"
+        public String getEmailAddress() { return email; }
+
+    }
+}
