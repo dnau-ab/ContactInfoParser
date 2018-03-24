b1dd1f0bba7315bcde36c6ae58c7c9566f7d4f98
David Naughton <dnau.ab@gmail.com>
2018-03-24 10:27:13 -0500

    Added support for middle initial, fixed small issue with phone pattern

--- ContactInfoParser/ContactInfoParser/Patterns.cs
+++ ContactInfoParser/ContactInfoParser/Patterns.cs
@@ -8,13 +8,13 @@ namespace ContactInfoParser
 {
     static class Patterns
     {
-
-        public const String name = @"[A-Z][A-Za-z]+[ \t]+([A-Z][A-Za-z]+(-[A-Za-z]+)?)";
+        // checks for first name, mi, last name (possibly hyphenated)
+        public const String name = @"[A-Z][A-Za-z]+[ \t]+([A-Z].?[ \t]+)?([A-Z][A-Za-z]+(-[A-Za-z]+)?)";
 
         // supports +# (###) ###-#### with or without the country code, parens, hyphen, or spaces
-        public const String phone = @"([+]\d+)?[ \t]*.?(\d\d\d).?[ \t]*(\d\d\d).?[ \t]*(\d\d\d\d)";
+        public const String phone = @"([+]\d+[ \t]*.?)?(\d\d\d).?[ \t]*(\d\d\d).?[ \t]*(\d\d\d\d)";
 
-        // emails may have other top level domains, but here I use some common ones
+        // emails may have other top level domains; here I use some common ones
         public const String email = @"[a-zA-Z]([a-zA-Z0-9].?)*[a-zA-Z0-9]@([a-zA-Z0-9].?[a-zA-Z0-9])+.(com|net|edu|org|gov)";
 
     }
